782e23aea9ef79186fe07e295202207398ee5f8a
KV.Dhanunjaya Redddy <KV.DhanunjayaReddy@godigit.com>
2024-08-07 17:30:03 +0530

    ReverseFilecontent

---
+++ FileOperations/ReverseFileContent.java
@@ -0,0 +1,41 @@
+package FileOperations;
+
+import java.io.*;
+
+public class ReverseFileContent {
+    public static void main(String[] args) {
+        BufferedReader reader = null;
+        BufferedWriter writer = null;
+
+        try {
+            reader = new BufferedReader(new FileReader("input.txt"));
+            StringBuilder content = new StringBuilder();
+            String line;
+
+            while ((line = reader.readLine()) != null) {
+                content.append(line).append("\n");
+            }
+
+            content.reverse();
+
+            writer = new BufferedWriter(new FileWriter("reversed.txt"));
+            writer.write(content.toString());
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+
+            try {
+                if (reader != null) {
+                    reader.close();
+                }
+                if (writer != null) {
+                    writer.close();
+                }
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+        }
+    }
+}
+
